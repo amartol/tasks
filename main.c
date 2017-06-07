6ec131ebeeefa079792c438e2ed1dfe3ac8e6baf
amartol <dipol.kb6@gmail.com>
2017-06-07 22:55:10 +0000

    task3

--- main.c
+++ main.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
 #include <stdint.h>
 
+
+uint64_t Converter(uint64_t);
+
 int main()
 {
-	uint16_t p = 1;
+uint64_t value = 0;
+printf("Enter a number\n");
+scanf("%lx", &value);
 
-	if(p >> 8) printf("BE"); 
-	else printf("LE");
+printf("value=%lx\n",value);
 
-	return 0;
+
+printf("Converted value=%lx\n",Converter(value));
+
+
+return 0;
+}
+
+uint64_t Converter(uint64_t num)
+{
+	uint64_t temp = 0;
+	while (num)
+    {
+      temp <<= 8;
+      temp |= (num & 0xff);
+      num >>= 8;
+    }
+    return temp;
 }
