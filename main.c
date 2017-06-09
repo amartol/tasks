a03c89930714a5f5380fd7f3ba52dafc031257e2
amartol <dipol.kb6@gmail.com>
2017-06-09 09:20:36 +0300

    add printf

--- main.c
+++ main.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <stdint.h>
 uint8_t res[]={26,65,234,31,25,25,25};
+
+
+int main()
+{
+	for(uint8_t st=0; st < 7; st++)
+		printf("st = %d, res =%d \n", st, res[st]);
+		
+	
+	return 0;
+}
